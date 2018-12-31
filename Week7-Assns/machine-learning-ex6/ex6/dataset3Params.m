9e04694cf927067dee2c0794af1187a1918b18f5
Shi Shi <j96shi@uwaterloo.ca>
2018-12-31 16:40:07 -0700

    Week 9 problems

--- Week7-Assns/machine-learning-ex6/ex6/dataset3Params.m
+++ Week7-Assns/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,12 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_arr = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_arr = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+err_matrix = zeros(length(C_arr),length(sigma_arr));
+for c = 1:length(C_arr)
+    for sig = 1:length(sigma_arr)
+        model= svmTrain(X, y, C_arr(c), @(x1, x2) gaussianKernel(x1, x2, sigma_arr(sig)));
+        predictions = svmPredict(model,Xval);
+        err_matrix(c,sig) = mean(double(predictions ~= yval));
+    endfor
+endfor
 
+disp(err_matrix);
 
-
-
-
+minimum = min(min(err_matrix));
+[C_index,sigma_index]=find(err_matrix==minimum);
+C = C_arr(C_index);
+sigma = sigma_arr(sigma_index);
+disp(sigma);
+disp(C);
 % =========================================================================
 
 end
